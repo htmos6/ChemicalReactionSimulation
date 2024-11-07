9dc0c95239f5a53b3fbad229777852df93ba3f57
htmos6 <mehmet.arslan4000@gmail.com>
2024-11-07 17:31:07 +0300

    ChemicalReaction class implemented with cv and mutex only

---
+++ ChemicalReactionSimulation/ChemicalReaction.cpp
@@ -0,0 +1,139 @@
+﻿#include "ChemicalReaction.h"
+
+ChemicalReaction::ChemicalReaction(int C, int N, int S, int Th, int O, int genRate) 
+{
+    this->genRate = genRate;
+    totalAtomNum = C + N + S + Th + O;
+    targetAtomNum[0] = C;
+    targetAtomNum[1] = N;
+    targetAtomNum[2] = S;
+    targetAtomNum[3] = Th;
+    targetAtomNum[4] = O;
+}
+
+void ChemicalReaction::Start() 
+{
+    std::thread co2_comp(&ChemicalReaction::Compose_CO2, this);
+    std::thread no2_comp(&ChemicalReaction::Compose_NO2, this);
+    std::thread so2_comp(&ChemicalReaction::Compose_SO2, this);
+    std::thread tho2_comp(&ChemicalReaction::Compose_THO2, this);
+
+    std::unique_lock<std::recursive_mutex> lck(mtx, std::defer_lock);
+
+    for (int i = 0; i < totalAtomNum; i++) 
+    {
+        int randAtomType = std::rand() % TOTAL_NUMBER_OF_ATOM_TYPE;
+
+        while (targetAtomNum[randAtomType] == 0) 
+        {
+            randAtomType = std::rand() % TOTAL_NUMBER_OF_ATOM_TYPE;
+        }
+
+        targetAtomNum[randAtomType] -= 1;
+
+        lck.lock();
+        initAtomNum[randAtomType] += 1;
+        std::cout << atomName[randAtomType] << " with ID: " << atomID++ << " is created." << std::endl;
+        lck.unlock();
+        cv.notify_all();
+
+        Sleep();
+    }
+
+    co2_comp.join();
+    no2_comp.join();
+    so2_comp.join();
+    tho2_comp.join();
+}
+
+void ChemicalReaction::Sleep() 
+{
+    double sTime = (-1.0 / 100.0) * log(1.0 - ((double)std::rand() / RAND_MAX));
+    std::this_thread::sleep_for(std::chrono::microseconds(static_cast<int>(sTime * 1000000)));
+}
+
+void ChemicalReaction::Compose_CO2() 
+{
+    while (true) 
+    {
+        std::unique_lock<std::recursive_mutex> lck(mtx);
+        while ((controller != 1 && controller != 3) || initAtomNum[0] < 1 || initAtomNum[4] < 2)
+        {
+            cv.wait(lck);
+        }
+
+        initAtomNum[0] -= 1;
+        initAtomNum[4] -= 2;
+        composedMolecule = "CO2";
+        DisplayComposedMolecule();
+
+        // Move to the next molecule in the sequence
+        controller = (controller == 1) ? 2 : 4;
+    }
+}
+
+void ChemicalReaction::Compose_NO2() 
+{
+    while (true) 
+    {
+        std::unique_lock<std::recursive_mutex> lck(mtx);
+        while (controller != 2 || initAtomNum[1] < 1 || initAtomNum[4] < 2) 
+        {
+            cv.wait(lck);
+        }
+
+        initAtomNum[1] -= 1;
+        initAtomNum[4] -= 2;
+        composedMolecule = "NO2";
+        DisplayComposedMolecule();
+
+        // Move to the next molecule in the sequence
+        controller = 3;
+    }
+}
+
+void ChemicalReaction::Compose_SO2() 
+{
+    while (true) 
+    {
+        std::unique_lock<std::recursive_mutex> lck(mtx);
+        while (controller != 4 || initAtomNum[2] < 1 || initAtomNum[4] < 2) 
+        {
+            cv.wait(lck);
+        }
+
+        initAtomNum[2] -= 1;
+        initAtomNum[4] -= 2;
+        composedMolecule = "SO2";
+        DisplayComposedMolecule();
+
+        // Move to the next molecule in the sequence
+        controller = 5;
+    }
+}
+
+void ChemicalReaction::Compose_THO2() 
+{
+    while (true) 
+    {
+        std::unique_lock<std::recursive_mutex> lck(mtx);
+        while (controller != 5 || initAtomNum[3] < 1 || initAtomNum[4] < 2) 
+        {
+            cv.wait(lck);
+        }
+
+        initAtomNum[3] -= 1;
+        initAtomNum[4] -= 2;
+        composedMolecule = "ThO2";
+        DisplayComposedMolecule();
+
+        // Reset to the start of the cycle
+        controller = 1;
+    }
+}
+
+void ChemicalReaction::DisplayComposedMolecule() 
+{
+    std::unique_lock<std::recursive_mutex> lck(mtx);
+    std::cout << "\nComposed Molecule: " << composedMolecule << "\n" << std::endl;
+}
---
+++ ChemicalReactionSimulation/ChemicalReaction.h
@@ -0,0 +1,50 @@
+﻿#ifndef INCLUDE_ATOM_H_
+#define INCLUDE_ATOM_H_
+
+#include <iostream>
+#include <cstdlib>
+#include <thread>
+#include <cmath>
+#include <condition_variable>
+#include <mutex>
+#include <map>
+
+#define TOTAL_NUMBER_OF_ATOM_TYPE 5
+
+struct atom
+{
+	int atomID;
+	char atomType; // C, N, S, or 
+};
+	
+inline std::string atomName[5] = { "C", "N", "S", "Th", "O" };
+
+class ChemicalReaction
+{
+public:
+	ChemicalReaction(int C, int N, int S, int Th, int O, int genRate);
+	~ChemicalReaction() = default;
+	void Start();
+
+private:
+	inline static int atomID = 0;
+	int totalAtomNum = 0;
+	int controller = 1; // 1 2 3 4 5 .... (*)
+	int genRate; // Generation interval for each atom
+	int targetAtomNum[5]; // Update at each production (*)
+	int initAtomNum[5] = { 0, 0, 0, 0, 0 }; // Update at each production (*)
+
+	std::condition_variable_any cv;
+	std::recursive_mutex mtx;
+	std::string composedMolecule;
+
+	void Sleep();
+	void Compose_CO2();
+	void Compose_NO2();
+	void Compose_SO2();
+	void Compose_THO2();
+	void DisplayComposedMolecule();
+
+};
+
+#endif
